3949415e64d93007c804512631fdb1a97e9d7dcc
michellekuo1208 <49825786+michellekuo1208@users.noreply.github.com>
2019-12-29 18:43:52 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+// file: script.js
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBPN-Kfwuwn7f4DRz3SJm-_VUjub44Io4Q",
+    authDomain: "ncut-20191222.firebaseapp.com",
+    databaseURL: "https://ncut-20191222.firebaseio.com",
+    projectId: "ncut-20191222",
+    storageBucket: "ncut-20191222.appspot.com",
+    messagingSenderId: "937416393167"
+
+
+};
+firebase.initializeApp(config);
+
+//create firebase database reference
+var dbRef = firebase.database();
+var contactsRef = dbRef.ref('contacts');
+
+//load older conatcts as well as any newly added one...
+contactsRef.on("child_added", function(snap) {
+  console.log("added", snap.key, snap.val());
+  $('#contacts').append(contactHtmlFromObject(snap.val()));
+});
+
+//save contact
+$('.addValue').on("click", function( event ) {  
+    event.preventDefault();
+    if( $('#name').val() != '' || $('#email').val() != '' ){
+      contactsRef.push({
+        name: $('#name').val().replace(/<[^>]*>/ig, ""),
+        email: $('#email').val().replace(/<[^>]*>/ig, ""),
+        location: {
+          city: $('#city').val().replace(/<[^>]*>/ig, ""),
+          state: $('#state').val().replace(/<[^>]*>/ig, ""),
+          zip: $('#zip').val().replace(/<[^>]*>/ig, "")
+        }
+      })
+      contactForm.reset();
+    } else {
+      alert('Please fill atlease name or email!');
+    }
+  });
+
+//prepare conatct object's HTML
+function contactHtmlFromObject(contact){
+  console.log( contact );
+  var html = '';
+  html += '<li class="list-group-item contact">';
+    html += '<div>';
+      html += '<p class="lead">'+contact.name+'</p>';
+      html += '<p>'+contact.email+'</p>';
+      html += '<p><small title="'+contact.location.zip+'">'+contact.location.city+', '+contact.location.state+'</small></p>';
+    html += '</div>';
+  html += '</li>';
+  return html;
+}
